54d9dad90e2ce2c947460d74eca036a80b31ab47
Bill <adamsw09@alumni.hanover.edu>
2014-08-11 22:24:46 -0400

    Improve readability slightly

--- life/life.py
+++ life/life.py
@@ -2,12 +2,12 @@ import sys, random, time
 
 boardSize = (10,10)
 
-while True: foo, bar, baz, globals()['board'] = None if globals().get('board') is None else [
+while True: foo, bar, baz, globals()['board'] = None if 'board' not in globals() else [
         (
             [sys.stdout.write('X' if cell else ' ') for cell in row],
             sys.stdout.write('\n')
         ) for row in board
-    ], time.sleep(1), sys.stdout.write('=' * boardSize[0]  +'\n'), [
+    ], time.sleep(1), sys.stdout.write('=' * boardSize[0] + '\n'), [
             [random.random() < 0.5 for i in range(boardSize[0])] for j in range(boardSize[1])
         ] if 'board' not in globals() else [
             map(
@@ -21,12 +21,12 @@ while True: foo, bar, baz, globals()['board'] = None if globals().get('board') i
                                     y0 in range(len(board)) and x0 in range(len(board[y0])) and board[y0][x0]
                                 ) for x0,y0 in (
                                     (x - 1, y - 1),
-                                    (x, y -1),
+                                    (x    , y - 1),
                                     (x + 1, y - 1),
-                                    (x - 1, y),
-                                    (x + 1, y),
+                                    (x - 1, y    ),
+                                    (x + 1, y    ),
                                     (x - 1, y + 1),
-                                    (x, y + 1),
+                                    (x    , y + 1),
                                     (x + 1, y + 1)
                                     )
                                 ]
